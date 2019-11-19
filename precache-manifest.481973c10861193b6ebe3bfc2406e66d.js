b712aee131068252fc3bea74e697c3ddafb3c2a1
Ashiqur Tusher <ashikurt77@gmail.com>
2019-11-20 00:48:17 +0600

    Updates

---
+++ precache-manifest.481973c10861193b6ebe3bfc2406e66d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7a2cc6918925be707d9934bf7f2a9d2",
+    "url": "/fuking-monsters/index.html"
+  },
+  {
+    "revision": "cdf038298b8ec39fa6c2",
+    "url": "/fuking-monsters/static/css/main.515663fb.chunk.css"
+  },
+  {
+    "revision": "5e69c3f4ddc3935b762c",
+    "url": "/fuking-monsters/static/js/2.4f66788d.chunk.js"
+  },
+  {
+    "revision": "cdf038298b8ec39fa6c2",
+    "url": "/fuking-monsters/static/js/main.b0c14154.chunk.js"
+  },
+  {
+    "revision": "f3bce1a0b511c90b9e52",
+    "url": "/fuking-monsters/static/js/runtime-main.1770fcd3.js"
+  }
+]);
